3299b24f2a32191f7a2a9e0c42c4d4afc3bd012f
Juno-whut <owenrupnow17@gmail.com>
2025-04-22 17:36:25 -0400

    Added new styles
    Added timeStyle, body Style, and heading style

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -7,3 +7,26 @@ const appBarStyle = TextStyle(
   fontWeight: FontWeight.w500,
   fontSize: 42
 );
+
+// heading style
+const headingStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w400,
+  fontSize: 32,
+);
+
+
+// time style
+const timeStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w500,
+  fontSize: 28,
+);
+
+
+// body style
+const bodyStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w300,
+  fontSize: 24,
+);
