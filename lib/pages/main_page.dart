41e4d403232329c759078934031351263bde410f
Juno-whut <owenrupnow17@gmail.com>
2025-04-22 16:20:05 -0400

    Figured Out Theme Issues
    Fixed Theme issues established color pallette for now and will start to work on Main Page UI

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -10,6 +10,16 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Alarm Clock'), titleTextStyle: TextStyle(color: Colors.white),),
+      body: Center(
+        child: Text(
+          'Alarm Clock', 
+          style: TextStyle(
+          color: Colors.white
+          ),
+        ),
+      ),
+    );
   }
 }
