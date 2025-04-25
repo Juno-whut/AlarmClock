bce565fc21c727b573915dc432a76d53f8158527
Juno-whut <owenrupnow17@gmail.com>
2025-04-25 19:36:36 -0400

    Added alarm checking logic in here since it was easier to put it in this file
    Future me is adding a way to stop alarms, toggle their status, make it so when you add or delete and song or alarm it refreshes so you see your changes

--- lib/pages/alarm_page.dart
+++ lib/pages/alarm_page.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
+
+import 'package:alarm_clock/constants/alarm_class.dart';
+import 'package:alarm_clock/services/alarm_service.dart';
 import 'package:alarm_clock/widgets/add_alarm_button.dart';
 import 'package:alarm_clock/widgets/alarm_clock_appbar.dart';
 import 'package:alarm_clock/widgets/alarm_list.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+Logger logger = Logger();
 
 class AlarmPage extends StatefulWidget {
   const AlarmPage({super.key});
@@ -11,9 +19,65 @@ class AlarmPage extends StatefulWidget {
 }
 
 class _AlarmPageState extends State<AlarmPage> {
+  AudioPlayer? audioPlayer;
+  List<AlarmList>? alarmList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    readAlarmJsonList().then((alarms) {
+      setState((){
+        audioPlayer = AudioPlayer();
+        alarmList = alarms;
+        startAlarmWatcher(alarms);
+      });
+    });
+  }
   @override
   void dispose() {
     super.dispose();
+  }
+
+  Future<void> playAlarmSong(String path) async {
+    try {
+      await audioPlayer!.stop();
+      await audioPlayer!.play(DeviceFileSource(path));
+      logger.d('Playing song from path: $path');
+
+      // Listen for when the song finishes playing
+      audioPlayer!.onPlayerComplete.listen((event) async {
+        logger.d('Song ended, replaying...');
+        await audioPlayer!.seek(Duration.zero);
+        await audioPlayer!.resume();
+      });
+
+    } catch (e) {
+      logger.e('Failed to play alarm song: $e');
+    }
+  }
+
+  // Alarm Watcher
+  void startAlarmWatcher(List<AlarmList> alarms) {
+    Timer.periodic(Duration(minutes: 1), (timer) async {
+      final now = TimeOfDay.now();
+      final hour = now.hourOfPeriod == 0 ? 12 : now.hourOfPeriod;
+      final minute = now.minute.toString().padLeft(2, '0');
+      final ampm = now.period == DayPeriod.am ? 'AM' : 'PM';
+      final formattedNow = '$hour:${minute.toString().padLeft(2, '0')}';
+
+      logger.d('Checking alarms at $formattedNow $ampm');
+
+    for (final alarm in alarms) {
+      logger.d('Checking alarm: ${alarm.time} ${alarm.ampm}');
+      if (alarm.isOn &&
+          alarm.time == formattedNow &&
+          alarm.ampm == ampm) {
+        logger.d('Triggering alarm: ${alarm.title}');
+        await playAlarmSong(alarm.songPath);
+        break;
+        }
+      }
+    });
   }
 
   @override
@@ -35,7 +99,7 @@ class _AlarmPageState extends State<AlarmPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            AlarmListWidget(),
+            AlarmListWidget(), 
             AddAlarmButton(),
           ],
         ),
